235925360e42b2bf2a807b3dfd7ae97b12cc7829
slxv# <nikaydokaym@gmail.com>
2024-11-28 11:13:49 +0000

    upd login page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form className="Box">
-					<p><input name="login" /></p>
-					<p><input name="password" /></p>
-					<p><input type="submit" value="Войти" /></p>
+			<div id="Box">
+        <form>
+					<text className="Text">Логин</text>
+					<p><input id="input" name="login" /></p>
+					<text className="Text">Пароль</text>
+					<p><input id="input" name="password" /></p>
+					<p><input className="Button" type="submit" value="Войти" /></p>	
 				</form>
+			</div>
 			</header>
     </div>
   );
