fa023a275bb19115e4c0ce62fc937e68b5b7c5c0
slxv# <nikaydokaym@gmail.com>
2024-11-28 10:11:28 +0000

    upd app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-					Hello there! app new
-		    </p>
-      </header>
+        <form className="Box">
+					<p><input name="login" /></p>
+					<p><input name="password" /></p>
+					<p><input type="submit" value="Войти" /></p>
+				</form>
+			</header>
     </div>
   );
 }
